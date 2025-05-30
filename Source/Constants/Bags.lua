497acc1db1aaf56b661b14cff82c08b51a885b7c
Charles Louw <charles.louw@gmail.com>
2025-05-30 15:32:36 +0200

    remove manifest files

---
+++ Source/Constants/Bags.lua
@@ -0,0 +1,51 @@
+INVENTORIX_BAGS_TYPES = {
+	-- Warband
+	[Enum.BagIndex.AccountBankTab_1] = "account",
+	[Enum.BagIndex.AccountBankTab_2] = "account",
+	[Enum.BagIndex.AccountBankTab_3] = "account",
+	[Enum.BagIndex.AccountBankTab_4] = "account",
+	[Enum.BagIndex.AccountBankTab_5] = "account",
+	-- Bank
+	[Enum.BagIndex.Bank] = "player",
+	[Enum.BagIndex.Reagentbank] = "player",
+	[Enum.BagIndex.BankBag_1] = "player",
+	[Enum.BagIndex.BankBag_2] = "player",
+	[Enum.BagIndex.BankBag_3] = "player",
+	[Enum.BagIndex.BankBag_4] = "player",
+	[Enum.BagIndex.BankBag_5] = "player",
+	[Enum.BagIndex.BankBag_6] = "player",
+	[Enum.BagIndex.BankBag_7] = "player",
+	-- Inventory
+	[Enum.BagIndex.Backpack] = "player",
+	[Enum.BagIndex.Bag_1] = "player",
+	[Enum.BagIndex.Bag_2] = "player",
+	[Enum.BagIndex.Bag_3] = "player",
+	[Enum.BagIndex.Bag_4] = "player",
+	[Enum.BagIndex.ReagentBag] = "player",
+}
+
+INVENTORIX_BANK_BAGS = {
+	Enum.BagIndex.AccountBankTab_1,
+	Enum.BagIndex.AccountBankTab_2,
+	Enum.BagIndex.AccountBankTab_3,
+	Enum.BagIndex.AccountBankTab_4,
+	Enum.BagIndex.AccountBankTab_5,
+	Enum.BagIndex.Bank,
+	Enum.BagIndex.Reagentbank,
+	Enum.BagIndex.BankBag_1,
+	Enum.BagIndex.BankBag_2,
+	Enum.BagIndex.BankBag_3,
+	Enum.BagIndex.BankBag_4,
+	Enum.BagIndex.BankBag_5,
+	Enum.BagIndex.BankBag_6,
+	Enum.BagIndex.BankBag_7,
+}
+
+INVENTORIX_INVENTORY_BAGS = {
+	Enum.BagIndex.Backpack,
+	Enum.BagIndex.Bag_1,
+	Enum.BagIndex.Bag_2,
+	Enum.BagIndex.Bag_3,
+	Enum.BagIndex.Bag_4,
+	Enum.BagIndex.ReagentBag,
+}
